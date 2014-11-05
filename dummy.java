cd8508d22ba4397f5fcee1a3c25cb2f650e20b37
tosabe <abe.toshiyuki@isfnet.com>
2014-11-05 17:30:41 +0900

    my second commit

---
+++ dummy.java
@@ -0,0 +1,25 @@
+/**
+ * ダミークラス
+ * 
+ * @author T.abe
+ * @since 2014/11/06
+ * @version 1.0
+ */
+public class Dummy extends BasicExecuteAction {
+	/*
+	 * 画面項目のチェック
+	 */
+	@Override
+	protected boolean chkItem(ActionMessages messages, ActionForm form, HttpServletRequest request) {
+		// 画面にチック項目は無いの場合、falseを戻る
+		return false;
+	}
+
+	protected String executeMethod(ActionForm form, HttpServletRequest request, HttpServletResponse response,
+			ActionMessages messages, Map<String, Object> mapInfo) {
+
+		return Constants.FORWARD_SUCCESS;
+	}
+
+}
+
